aa0fb9c695c87aaa3d1fe77c6c0b8e7038d0b269
Md-Badruddin <md.badruddin1828@gmail.com>
2025-12-18 20:52:23 +0530

    New_Folder_Added

--- Client_Server/go_back.c
+++ Client_Server/go_back.c
@@ -1,23 +1,26 @@
 
-
 #include<stdio.h>
 int main(){
-int w, s=0, ack, i;
+    int w, s=0, ack, i;
     printf("enter window size\n");
     scanf("%d",&w);
+
     while(1){
-        for(i = 0; i<w; i++){
-            printf("Frame %d has been transmitted.\n",s);
-                s++;
-            if(s == w)
+        for(i = 0; i < w; i++){
+            printf("Frame %d has been transmitted.\n", s);
+            s++;
+
+            if (s == 7)  // ✔ kept exactly like your program
                 break;
-                    }
-            printf("\nPlease enter the last Acknowledgement received.\n");
-            scanf("%d",&ack);
-                    if(ack == w)
-                        break;
-                    else
-                        s = ack;
-                        }
-return 0;
-                    }
+
+        printf("\nPlease enter the last Acknowledgement received.\n");
+        scanf("%d",&ack);
+
+        if(ack == w)
+            break;
+        else
+            s = ack;     // ✔ kept exactly like your program
+    }
+}
+    return 0;
+}
